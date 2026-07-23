1d5255c0f5e86d5a6dbe9ec3421248d0cf1c3dd2
Joshua <77cj5gzmc6@privaterelay.appleid.com>
2026-07-23 18:03:52 -0300

    Add expense handling to main.py

--- main.py
+++ main.py
@@ -8,3 +8,19 @@ print("Welcome ! Today is", today.strftime("%d/%m/%Y"))
 
 data = load_data()
 expenses = data["expenses"]
+
+while True:
+  print("""
+  1. Add expense
+  2. Statistics (coming soon)
+  3. Exit
+  """)
+  choice=input("> ")
+
+if choice=="1":
+  expenses = add_expense(expenses)
+  data["expenses"] = expenses
+  save_data(data)
+
+elif choice=="3":
+  break
