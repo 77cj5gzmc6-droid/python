c6daf86d28906925a2b55664fc953d7c27f5b4df
Joshua <77cj5gzmc6@privaterelay.appleid.com>
2026-07-23 18:17:06 -0300

    Refactor expense management and user interaction

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 from datetime import datetime, date
 import json
 import os
-from database import load_data, save_data
+
+FILE = "/Users/joshuawhite-labbe/Documents/GitHub/python/finances.json"
+
+def load_data():
+    if os.path.exists(FILE):
+        with open(FILE, "r", encoding="utf-8") as ft:
+            return json.load(ft)
+    else:
+        return {
+            "salary": 0,
+            "expenses": []
+        }
+
+def save_data(data):
+    with open(FILE, "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=4, ensure_ascii=False)
+
+#########################
+## Welcome
+#########################
 
 today = datetime.now()
 print("Welcome ! Today is", today.strftime("%d/%m/%Y"))
@@ -9,18 +28,142 @@ print("Welcome ! Today is", today.strftime("%d/%m/%Y"))
 data = load_data()
 expenses = data["expenses"]
 
+#########################
+## Salary
+#########################
+
+if data["salary"]==0:
+    salary = float(input("What is your monthly salary? "))
+    payday = float(input("What is your payday? Please enter the day of the month (eg. 1 if its on the first of the month)"))
+    data["salary"] = salary
+    data["payday"] = payday
+    save_data(data)
+else:
+    salary = data["salary"]
+    print("Your monthly salary is", salary, "pounds.")
+
+if date.today().day == 1:
+    print("It's the first day of the month! Your expenses have been reset.")
+    expenses = []
+    data["expenses"] = expenses
+    save_data(data)
+
+########################
+## Expenses
+########################
+
+money = salary - sum(
+    expense["amount"] for expense in expenses
+)
+print ( "You have", money, "pounds left this month.")
+
+if date.today().day == data["payday"]:
+    print("It's payday! Your salary has been added to your account.")
+    money += salary
+    print ( "You have", money, "pounds left this month.")
+
+########################
+## Main menu
+########################
+
 while True:
-  print("""
-  1. Add expense
-  2. Statistics (coming soon)
-  3. Exit
-  """)
-  choice=input("> ")
+    action = input("What would you like to do? (New expense (n) / Delete an expense (s) / Add money (a) / Modify an expense (md) / View my expenses (v) / Search for an expense (r) / Check remaining (e) / Modify my monthly salary (ms) / Quit (q)")
+    
+    if action == "n":
+        category = input("What is the category of your expense? ")
+        name = input("What is the name of your expense? Attention, it cannot be repeated ")
+        amount = float(input("What is the amount of your expense? "))
+        date = input("What is the date of your expense? (dd/mm/yyyy) ")
+        money = money - amount
+        expense = {
+            "name": name,
+            "date": date,
+            "category": category,
+            "amount": amount
+        }
+        expenses.append(expense)
+        data["expenses"] = expenses
+        save_data(data)
+        print("Your expense has been added successfully. You have", money, "pounds left this month.")
+    
+    elif action == "v":
+        print("Here is your list of expenses:")
+        for expense in expenses:
+            print(expense)
+    
+    elif action == "e":
+        print ("You have", money, "pounds left this month.")
+    
+    elif action == "ms":
+        salary = float(input("What is your new monthly salary?"))
+        data["salary"] = salary
+        save_data(data)
+        print("Your new monthly salary is now", salary, "pounds.")
+    
+    elif action == "q":
+        print("Thank you for using our application. Have a nice day!")
+        break
 
-if choice=="1":
-  expenses = add_expense(expenses)
-  data["expenses"] = expenses
-  save_data(data)
+    elif action == "s":
+        name = input("What is the name of the expense you want to delete? ")
+        expense_found = False
+        for expense in expenses:
+            if expense["name"] == name:
+                expenses.remove(expense)
+                data["expenses"] = expenses
+                save_data(data)
+                money += expense["amount"]
+                print("The expense has been deleted successfully. You have", money, "pounds left this month.")
+                expense_found = True
+                break
+        if not expense_found:
+                print("No expense found with that name.")
 
-elif choice=="3":
-  break
+    elif action == "md":
+        name = input("What is the name of the expense you want to modify?")
+        expense_found = False
+        for expense in expenses:
+            if expense["name"] == name:
+                new_category = input("What is the new category of your expense? ")
+                if new_category == "m":
+                    new_category = expense["category"]
+                new_name = input("What is the new name of your expense? Beware, it cannot be repeated ")
+                if new_name == "m":
+                    new_name = expense["name"]
+                new_amount = float(input("What is the new amount of your expense? Press 0 if it's the same amount"))
+                if new_amount == 0:
+                    new_amount = expense["amount"]
+                new_date = input("What is the new date of your expense? (dd/mm/yyyy) ")
+                if new_date == "m":
+                    new_date = expense["date"]
+                money += expense["amount"] - new_amount
+                expense["category"] = new_category
+                expense["name"] = new_name
+                expense["amount"] = new_amount
+                expense["date"] = new_date
+                data["expenses"] = expenses
+                save_data(data)
+                print("The expense has been modified successfully. You have", money, "pounds left this month.")
+                expense_found = True
+                break
+        if not expense_found:
+                print("No expense found with that name.")
+
+    elif action == "r":
+        name = input("What is the name of the expense you want to search for?")
+        expense_found = False
+        for expense in expenses:
+            if expense["name"] == name:
+                print("Expense found. Name =", expense["name"], "Category =", expense["category"], "Amount =", expense["amount"], "Date =", expense["date"])
+                expense_found = True
+                break
+        if not expense_found:
+                print("No results found.")
+
+    elif action == "a":
+        amount_added = float(input("How much money do you want to add? "))
+        money += amount_added
+        print("You have added", amount_added, "pounds. You have", money, "pounds left this month.")
+
+    else:
+        print("Invalid action. Please try again.")
